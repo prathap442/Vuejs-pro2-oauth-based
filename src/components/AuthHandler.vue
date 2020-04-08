24e8829623b9cbf5556bf915b95e4c49ccc064bd
= <=>
2020-04-08 14:33:57 +0530

    calling finalized login action in modules/AuthHandler.vue

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -4,10 +4,15 @@
   </div>
 </template>
 <script>
+  import { mapActions } from 'vuex';
   export default({
     name: "AuthHandler",
+    methods: 
     created(){
-        
+      // by importing mapActions we have a method of availability here 
+      // by name finalizedLogin and the actions in in the mapActions that are made available
+      // to the methods can be called by using this.finalizedLogin
+      this.finalizedLogin();
     }  
   })
 </script>
